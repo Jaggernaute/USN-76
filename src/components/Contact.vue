54b3ed5c8816716b953a9f2a0c986afc69114569
sigmanificient <sigmanificient@gmail.com>
2021-12-24 23:43:50 +0100

    :sparkles: Contact component

---
+++ src/components/Contact.vue
@@ -0,0 +1,59 @@
+<script>
+
+export default {
+  props: ['email', 'tel']
+}
+</script>
+
+<template>
+  <div id="contact" class="contact">
+    <h1>C<span class="red-underline">ontac</span>t</h1>
+    <div class="contact-wrapper">
+      <p class="mail">
+        <a v-bind:href="'mailto:' + email">
+          {{ email }}
+        </a>
+      </p>
+      <p class="phone">
+        <a v-bind:href="'tel:' + tel">
+          {{ tel }}
+        </a>
+      </p>
+    </div>
+    <iframe
+        title="google map" src="https://maps.google.com/maps?q=Le+Mesnil-Rhéaume&amp;output=embed">
+    </iframe>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.contact {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  &-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    p {
+      margin: 0.5em;
+
+      a {
+        color: #000;
+        text-decoration: none;
+      }
+    }
+  }
+
+  iframe {
+    width: 100%;
+    aspect-ratio: 69/42;
+  }
+}
+
+</style>
+
