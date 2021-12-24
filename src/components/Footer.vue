b7d359c083aaa4c7a66c0c3b38881a8276a12ca9
sigmanificient <sigmanificient@gmail.com>
2021-12-24 23:44:17 +0100

    :sparkles: Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <p>&copy; Copyright USN76 - Touts droits réservés.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+footer {
+  box-shadow: 0 -0.5rem 1rem rgba(0, 0, 0, 0.1);
+  padding: 20px;
+}
+
+footer p {
+  text-align: center;
+}
+</style>
